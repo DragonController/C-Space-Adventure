f492a14d8a8a44fbd1cfad3f8dd3bd36434e39e5
DragonController <dan.h.fowler@gmail.com>
2021-05-23 15:39:45 -0700

    Modified program to use cookbook approach

--- main.c
+++ main.c
@@ -7,33 +7,75 @@
 #include <stdlib.h>
 #include <string.h>
 
+void printWelcome();
+char* responseFor(char* output);
+void printGreeting(char* name);
+int randomTravel();
+void travelToRandomPlanet();
+void travelTo(char* planet);
+
 int main(int argc, char** argv)
 {
-  char* input = malloc(256);
+  printWelcome();
+  printGreeting(responseFor("What is your name?"));
+  if (randomTravel()) {
+    travelToRandomPlanet();
+  } else {
+    travelTo(responseFor("Name the planet you would like to visit."));
+  }
+}
+
+void printWelcome()
+{
   printf("Welcome to the Solar System!\n");
   printf("There are 9 planets to explore.\n");
-  printf("What is your name?\n");
+}
+
+char* responseFor(char* output)
+{
+  char* input = malloc(256);
+  printf("%s\n", output);
   fgets(input, 256, stdin);
   if ((strlen(input) > 0) && (input[strlen(input) - 1] == '\n')) {
     input[strlen(input) - 1] = '\0';
   }
-  printf("Nice to meet you, %s. My name is Grace.\n", input);
+  free(input);
+  return input;
+}
+
+void printGreeting(char* name)
+{
+  printf("Nice to meet you, %s. My name is Grace.\n", name);
   printf("Let's go on an adventure!\n");
-  while (!(strcmp(input, "Y") == 0 || strcmp(input, "N") == 0)) {
+}
+
+int randomTravel()
+{
+  char* input = malloc(256);
+  while (1) {
     printf("Shall I randomly choose a planet for you to visit? (Y or N)\n");
     fgets(input, 256, stdin);
     if ((strlen(input) > 0) && (input[strlen(input) - 1] == '\n')) {
       input[strlen(input) - 1] = '\0';
     }
-    if (strcmp(input, "N") == 0) {
-      printf("Name the planet you would like to visit.\n");
-	  scanf("%s", input);
-	  strcpy(input, "N");
-    } else if (strcmp(input, "Y") != 0) {
+    if (strcmp(input, "Y") == 0) {
+      return 1;
+    } else if (strcmp(input, "N") == 0) {
+      return 0;
+    } else {
       printf("Sorry, I didn't get that.\n");
 	}
   }
+}
+
+void travelToRandomPlanet()
+{
   printf("Mercury\n");
   printf("A very hot planet, closest to the sun.");
-  free(input);
+}
+
+void travelTo(char* planet)
+{
+  printf("Mercury\n");
+  printf("A very hot planet, closest to the sun.");
 }
